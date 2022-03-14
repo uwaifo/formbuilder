c36a7aef6e81e9ea9f88e22f58c727e35a7458b4
uwaifo <mr.uwaifo@gmail.com>
2022-03-14 16:21:08 +0100

    04. Handling multiple fields

--- form/fields.go
+++ form/fields.go
@@ -1,5 +1,7 @@
 package form
 
+import "reflect"
+
 type field struct {
 	Label       string
 	Name        string
@@ -7,14 +9,41 @@ type field struct {
 	Placeholder string
 }
 
-func fields(strct interface{}) field {
+func fields(strct interface{}) []field {
 
-	return field{
-		Label:       "Name",
-		Name:        "Name",
-		Type:        "text",
-		Placeholder: "Name",
+	rv := reflect.ValueOf(strct)
+	t := rv.Type()
+
+	var ret []field
+	for i := 0; i < t.NumField(); i++ {
+		tf := t.Field(i)
+		f := field{
+			Label:       tf.Name,
+			Name:        tf.Name,
+			Type:        "text",
+			Placeholder: tf.Name,
+		}
+
+		ret = append(ret, f)
+
 	}
+	return ret
+	//tf := t.Field(0)
+
+	// return []field{{
+	// 	Label:       tf.Name,
+	// 	Name:        tf.Name,
+	// 	Type:        "text",
+	// 	Placeholder: tf.Name,
+	// },
+	// }
+
+	// return field{
+	// 	Label:       "Name",
+	// 	Name:        "Name",
+	// 	Type:        "text",
+	// 	Placeholder: "Name",
+	// }
 
 }
 
--- form/fields_test.go
+++ form/fields_test.go
@@ -2,6 +2,7 @@ package form
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -9,24 +10,64 @@ func TestFields(t *testing.T) {
 
 	tests := []struct {
 		strct interface{}
-		want  field
+		want  []field
 	}{
 		{
 			strct: struct {
 				Name string
 			}{},
-			want: field{
+			want: []field{{
 				Label:       "Name",
 				Name:        "Name",
 				Type:        "text",
 				Placeholder: "Name",
+			}},
+		},
+		{
+			strct: struct {
+				FullName string
+			}{},
+			want: []field{{
+				Label:       "FullName",
+				Name:        "FullName",
+				Type:        "text",
+				Placeholder: "FullName",
+			},
+			},
+		},
+		// NOTE
+		{
+			strct: struct {
+				Name  string
+				Email string
+				Age   int
+			}{},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+				},
+				{
+					Label:       "Email",
+					Name:        "Email",
+					Type:        "text",
+					Placeholder: "Email",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+				},
 			},
 		},
 	}
 	for _, tc := range tests {
-		t.Run(fmt.Sprintf("%v", tc.strct), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%T", tc.strct), func(t *testing.T) {
 			got := fields(tc.strct)
-			if got != tc.want {
+			if !reflect.DeepEqual(got, tc.want) {
 				t.Errorf("fields() = %v; want %v", got, tc.want)
 			}
 		})
